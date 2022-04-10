637b1162b8eeea956b4f8ab023530382356aa10a
Jeff White <44498114+jeffwhite530@users.noreply.github.com>
2022-04-10 10:12:03 -0400

    Accept compute node SSH keys in Ansible

--- scripts/cluster-bootstrap.sh
+++ scripts/cluster-bootstrap.sh
@@ -5,6 +5,8 @@
 
 cat clusterParametersSingleQuotes.json | tr "'" '"' > clusterParameters.json
 
+export ANSIBLE_HOST_KEY_CHECKING=False
+
 ansible-playbook -i 127.0.0.1, --limit 127.0.0.1 --connection=local ./ansible/cluster-headvm-config.yaml
 
 sleep 5
